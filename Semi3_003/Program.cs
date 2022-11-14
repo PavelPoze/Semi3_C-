5d441c42332f39a0c999902cb7e5087beec0659d
PavelPoz <79132207654@yandex.ru>
2022-11-14 10:44:10 +0700

    Нахлждения индекса заданного числа

---
+++ Semi3_003/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 22, 2, 4, 223, 54, 8, 765, 4 };
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index]== find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+   // index = index =1;
+    index++;
+}
