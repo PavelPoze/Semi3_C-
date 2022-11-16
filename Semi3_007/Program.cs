e2801463d182324bf2d60797235bfe0a9934392c
PavelPoz <79132207654@yandex.ru>
2022-11-16 12:23:35 +0700

    Посик чисел и иной метод оформления через функцию

--- Semi3_007/Program.cs
+++ Semi3_007/Program.cs
@@ -1,11 +1,11 @@
 ﻿//Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
 // Если число 1 не кратно числу 2, то программа выводит остаток от деления.
 
-Console.WriteLine("Введите число1: ");
-int numb1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число2: ");
-int numb2 = Convert.ToInt32(Console.ReadLine());
-//int ost = 
+
+int numb1 = ReadInt("Введите первое число: ");
+int numb2 = ReadInt("Введите второе число: ");
+//int remante = numb1 % numb2;
+//if (remanted ==0)
 if (numb1%numb2 !=0)     // !=0 значит не равно 0. т.е не кратно
 {
     Console.WriteLine("НЕ кратно, остаток: " + numb1%numb2);
@@ -15,3 +15,8 @@ else
     Console.WriteLine("Кратно");
 }
 // работает
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
