1212efbc830049a4aa0da85f9e4e2d43e74b58de
PavelPoz <79132207654@yandex.ru>
2022-11-16 12:57:56 +0700

    Решение задач на уроке

---
+++ Semi3_008/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+7 и 23.  */
+/*
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%7 ==0 && number%23 ==0)
+{
+    Console.WriteLine("Да");
+
+}
+else
+{
+    Console.WriteLine("Нет");
+}  */ // work
+
+/*Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8, 9  ->  нет
+*/
+/*
+Console.WriteLine("Введите число1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number*number==number2 || number2*number2==number)
+{
+    Console.WriteLine("Да");
+
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+*/ // work
