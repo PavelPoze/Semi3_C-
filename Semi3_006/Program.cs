21df83f98ab5b01156a6c9cac533e43b5117725f
PavelPoz <79132207654@yandex.ru>
2022-11-16 09:00:48 +0700

    Вротрая задача с сенинара

---
+++ Semi3_006/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98 */
+
+int randomNumber = new Random().Next(100, 1000);
+int lastDigit = randomNumber % 10;                  // нахождение первого числа
+int firstDigit = randomNumber / 100;              // нахождение второго числа     
+Console.WriteLine(randomNumber);
+
+Console.Write(firstDigit);    //Console.WriteLine(firstDigit + "" + lastDigit);
+Console.WriteLine(lastDigit);
+
+
