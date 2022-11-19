ae72f3a981ff58e0c9c0a0eb607f58403d11f3c1
PavelPoze <79132207654@yandex.ru>
2022-11-19 10:10:56 +0700

    Zadacha 1

---
+++ Semi3_301/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.*/
+while (true)   // добавили условия если пользователь ввел числа не верные,
+               // то программа будет запрашивать числа пока не введут правильные значения
+{
+    int x = ReadInt("Введите точку x: ");
+    int y = ReadInt("Введите точку y: ");
+    if (x == 0 || y == 0)
+    {
+        Console.WriteLine("x и y не могут быть равны 0");
+        //return;  //выход из программы
+        continue; // пропускает цикл и идет дальше
+    }
+
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine(" 1 ");
+    }
+    if (x < 0 && y > 0)
+    {
+        Console.WriteLine(" 2 ");
+    }
+    if (x < 0 && y < 0)
+    {
+        Console.WriteLine(" 3 ");
+    }
+    if (x > 0 && y < 0)
+    {
+        Console.WriteLine(" 4 ");
+    }
+    break; // остановиит цикл while
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
